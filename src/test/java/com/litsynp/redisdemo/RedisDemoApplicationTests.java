e374edbf007b0783dd65267d5878f16db1b53645
litsynp <nocte55is@gmail.com>
2022-06-18 12:11:43 +0900

    build: init project

---
+++ src/test/java/com/litsynp/redisdemo/RedisDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.litsynp.redisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
