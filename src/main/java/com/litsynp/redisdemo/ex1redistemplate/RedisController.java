52c7b84ea955f644eb5732d727a37b8ee6e81389
litsynp <nocte55is@gmail.com>
2022-06-18 23:26:11 +0900

    feat: change API URI

--- src/main/java/com/litsynp/redisdemo/ex1redistemplate/RedisController.java
+++ src/main/java/com/litsynp/redisdemo/ex1redistemplate/RedisController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/str-str")
+@RequestMapping("/v1/str-str")
 @RequiredArgsConstructor
 public class RedisController {
 
--- src/main/java/com/litsynp/redisdemo/ex3cache/MemberCacheTestController.java
+++ src/main/java/com/litsynp/redisdemo/ex3cache/MemberCacheTestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/members")
+@RequestMapping("/v3/members")
 @Slf4j
 public class MemberCacheTestController {
 
